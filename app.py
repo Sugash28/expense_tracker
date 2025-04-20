3b0afa53b9b6c2d13a177ea646adb526169d4195
sug <sugashsugu@gmail.com>
2025-04-20 09:46:55 +0530

    db

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
 from db import get_db_connection, initialize_database
-from generate_pdf import create_expense_pdf
+from pdf_generator import create_expense_pdf
 
 app = Flask(__name__)
 CORS(app)
